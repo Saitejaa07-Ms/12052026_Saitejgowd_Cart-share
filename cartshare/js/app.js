f4e95c823353dfc5968ca2c9eca21f15e7df127f
Saiteja <saitejgwd@gmail.com>
2026-06-17 20:06:36 +0530

    Update app.js

--- cartshare/js/app.js
+++ cartshare/js/app.js
@@ -209,3 +209,22 @@ function onRoomUpdated(callback) {
     }
   });
 }
+const firebaseConfig = {
+  apiKey: "AIzaSyDFNM0-52TR2QA-VhIqkg50fAP4WYTNFBI",
+  authDomain: "cartshare-6bee0.firebaseapp.com",
+  databaseURL: "https://cartshare-6bee0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cartshare-6bee0",
+  storageBucket: "cartshare-6bee0.firebasestorage.app",
+  messagingSenderId: "141861539109",
+  appId: "1:141861539109:web:5821f15769f1f388239ecc"
+};
+
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+
+// SUBMIT KI OKKA ROOM FIX CHESAM
+const ROOM = "FINAL2026";
+window.sharedCartRef = firebase.database().ref('rooms/' + ROOM + '/items');
+
+// Cart functions
+window.addItem = (name) => sharedCartRef.push({name: name, time: Date.now()});
+window.showCart = (callback) => sharedCartRef.on('value', snap => callback(snap.val()));
