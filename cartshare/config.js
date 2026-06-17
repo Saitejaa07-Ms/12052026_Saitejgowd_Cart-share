79f259db8535aae5fcca4613a949ea286a093dc5
Saiteja <saitejgwd@gmail.com>
2026-06-17 19:31:29 +0530

    Create config.js

---
+++ cartshare/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFNM8-52TR2QA-VhIqkg50fAP4WYTNFBI",
+  authDomain: "cartshare-6bee0.firebaseapp.com",
+  databaseURL: "https://cartshare-6bee0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cartshare-6bee0",
+  storageBucket: "cartshare-6bee0.firebasestorage.app",
+  messagingSenderId: "141861539109",
+  appId: "1:141861539109:web:5821f15769f1f388239ecc"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
